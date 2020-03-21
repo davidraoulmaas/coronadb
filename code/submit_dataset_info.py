8877a8705fd54995f72a64e9fbe8e580bc3c5e61
Darina Goldin <darina@dojomadness.com>
2020-03-21 21:25:01 +0100

    Better resolution of paths

--- code/submit_dataset_info.py
+++ code/submit_dataset_info.py
@@ -1,21 +1,24 @@
 # script to easily have a standartised lookup of all datasets in the repo
 # since descriptions might well contain commata, the resulting csv is tab-separated
 
-from pandas import read_csv
 import sys
 
+# check python version before importing pathlib. python3 is a requirement due to usage of pathlib and different
+# behaviour of input in python 2
 if sys.version_info[0] < 3:
     raise Exception("Python 3 is required.")
 
-# load old data so we can check for duplicates
-# this try-except is horrible and needs to be replaced by a better way of dealing with paths
-try:
-    old_data = read_csv('../data/overview_of_datasets.csv', sep='\t')
-    data_path = '../data/overview_of_datasets.csv'
-except IOError:
-    old_data = read_csv('data/overview_of_datasets.csv', sep='\t')
-    data_path = 'data/overview_of_datasets.csv'
+from pandas import read_csv
+from pathlib import Path
 
+# get path the dataset
+proj_dir = Path(__file__).absolute().parent.parent
+file_path = Path.joinpath(proj_dir, 'data', 'overview_of_datasets.csv')
+
+# load existing data to check for duplicate submissions
+old_data = read_csv(file_path, sep='\t')
+
+# ask for user input of metadata
 name = input('Name of the dataset:\n')
 while name == '':
      print('Name required. Please try again.\n')
@@ -40,9 +43,10 @@ license = input('Dataset license:\n')
 
 other = input('Any other information about the dataset:\n')
 
+# append user entered data to the csv
 csv_row = '\t'.join([name, status, source, description, license]) + '\n'
-with open(data_path,'a') as f:
-     f.write(csv_row)
+with file_path.open('a') as f:
+    f.write(csv_row)
 
 print('Success!')
 
