fc83111db3b1c2daded2487ca728c061107f7beb
Darina Goldin <darina@dojomadness.com>
2020-03-21 19:50:02 +0100

    Enforce python3 and fix paths error

--- code/submit_dataset_info.py
+++ code/submit_dataset_info.py
@@ -2,9 +2,19 @@
 # since descriptions might well contain commata, the resulting csv is tab-separated
 
 from pandas import read_csv
+import sys
+
+if sys.version_info[0] < 3:
+    raise Exception("Python 3 is required.")
 
 # load old data so we can check for duplicates
-old_data = read_csv('../data/overview_of_datasets.csv', sep='\t')
+# this try-except is horrible and needs to be replaced by a better way of dealing with paths
+try:
+    old_data = read_csv('../data/overview_of_datasets.csv', sep='\t')
+    data_path = '../data/overview_of_datasets.csv'
+except IOError:
+    old_data = read_csv('data/overview_of_datasets.csv', sep='\t')
+    data_path = 'data/overview_of_datasets.csv'
 
 name = input('Name of the dataset:\n')
 while name == '':
@@ -31,7 +41,7 @@ license = input('Dataset license:\n')
 other = input('Any other information about the dataset:\n')
 
 csv_row = '\t'.join([name, status, source, description, license]) + '\n'
-with open('../data/overview_of_datasets.csv','a') as f:
+with open(data_path,'a') as f:
      f.write(csv_row)
 
 print('Success!')
